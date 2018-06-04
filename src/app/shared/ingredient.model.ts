9ec08fb3fa8e9a0f76d68d34ce9da08cacf01259
Chloe Corrigan <ccorrigan@marginfactor.com>
2018-06-04 14:45:51 -0400

    shopping list and edit option layout

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,13 @@
+export class IngredientModel {
+	//define structure of ingredient model object
+	// public name: string;
+	// public amount: number;
+
+	// constructor(name: string, amount: number) {
+	// 	this.name = name;
+	// 	this.amount = amount;
+	// }
+
+	//simplified version below!
+	constructor(public name: string, public amount: number) {}
+}
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { IngredientModel } from '../shared/ingredient.model';
+
 @Component({
   selector: 'shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
+	ingredients: IngredientModel[] = [
+	    //test ingredients
+		new IngredientModel('Apples', 5),
+		new IngredientModel('Tomatoes', 2)
+	];
 
   constructor() { }
 
