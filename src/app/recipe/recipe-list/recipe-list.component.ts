a70b028b23fd9b372ed47ff278379665224e79a7
Chloe Corrigan <ccorrigan@marginfactor.com>
2018-06-04 13:52:24 -0400

    recipe components prepped

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { RecipeModel } from '../recipe.model';
+
 @Component({
   selector: 'recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit{
+  
+  //define this as specifically a recipe model array so that it will recognize objects and only allow objects of specified model	
+  recipes: RecipeModel[] = [
+
+   //an example recipe
+  new RecipeModel('test recipe', 'test desc', 'https://martensitakinenglish.files.wordpress.com/2014/03/printable-recipe-cards-martensitak.png')
+
+  ];
+
+  
 
   constructor() { }
 
