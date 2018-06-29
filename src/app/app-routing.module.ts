140d21d29cbd3d85ec3ab0844f42555677e896ef
Chloe Corrigan <ccorrigan@marginfactor.com>
2018-06-29 13:57:43 -0400

    added recipe-edit route and component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 //child-pages
 import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
 import { RecipeNotSelectedComponent } from './recipe/recipe-not-selected/recipe-not-selected.component';
+import { RecipeEditComponent } from './recipe/recipe-edit/recipe-edit.component';
 
 const appRoutes: Routes = [
 	//pathMatch must be used to redirect empty path; it specifies to only redirect if the full path is empty; the default is 'prefix'
@@ -15,7 +16,10 @@ const appRoutes: Routes = [
 			children: [
 			  //here, this empty path references a path with just /recipes.  In this case, if there are no recipes displayed via :id, 
 			  {path: '', component: RecipeNotSelectedComponent},
-			  {path: ':id', component: RecipeDetailComponent }
+			  //IMPORTANT!  any dynamic route (in this case, :id) must come AFTER any other relative child routes (otherwise, 'new' will try to be interpreted as an :id) 
+			  {path: 'new', component: RecipeEditComponent},
+			  {path: ':id', component: RecipeDetailComponent },
+			  {path: ':id/edit', component: RecipeEditComponent}
 			]
 	},
 	{path: 'shopping-list', component: ShoppingListComponent}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+
+import { DropdownDirective } from './shared/dropdown.directive';
+
+import { ShoppingListService } from './shopping-list/shopping-list.service';
+
+//for routing
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
@@ -11,13 +19,8 @@ import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component'
 import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipe/recipe-list/recipe-item/recipe-item.component';
 import { RecipeNotSelectedComponent } from './recipe/recipe-not-selected/recipe-not-selected.component';
+import { RecipeEditComponent } from './recipe/recipe-edit/recipe-edit.component';
 
-import { DropdownDirective } from './shared/dropdown.directive';
-
-import { ShoppingListService } from './shopping-list/shopping-list.service';
-
-//for routing
-import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
@@ -31,7 +34,8 @@ import { AppRoutingModule } from './app-routing.module';
     RecipeDetailComponent,
     RecipeItemComponent,
     DropdownDirective,
-    RecipeNotSelectedComponent
+    RecipeNotSelectedComponent,
+    RecipeEditComponent
   ],
   imports: [
     BrowserModule,
