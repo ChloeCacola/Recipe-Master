200df3439d8fc2f39db63386369fe1d9fa484d9b
Chloe Corrigan <ccorrigan@marginfactor.com>
2018-07-18 16:11:15 -0400

    unsubscribe added

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { RecipeModel } from '../recipe.model';
-
 import { RecipeService } from '../recipe.service';
+
 
 @Component({
   selector: 'recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit{
+export class RecipeListComponent implements OnInit, OnDestroy {
   
+  //defining for unsubscribing..
+  subscription: Subscription;
   //define this as specifically a recipe model array so that it will recognize objects and only allow objects of specified model	
   //moved to recipe.service (delete)
   recipes: RecipeModel[];
@@ -21,13 +24,18 @@ export class RecipeListComponent implements OnInit{
   //define recipes with recipe service
   ngOnInit() {
     //subscribing to any recipe updates
-    this.recipeService.recipesChanged.subscribe(
+    this.subscription = this.recipeService.recipesChanged.subscribe(
       (recipesUpdated: RecipeModel[])=> {
         this.recipes = recipesUpdated;
       });
 
     //getting the recipes from recipe service (where recipes are stored and handled)
   	this.recipes = this.recipeService.getRecipes();
+  }
+
+  ngOnDestroy() {
+    //unsubscribe to the recipes changed service - prevent memory leaks
+    this.subscription.unsubscribe();
   }
 
 }
