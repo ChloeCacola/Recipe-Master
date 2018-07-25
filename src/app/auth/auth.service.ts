5c987fd133e0cc282b5690e31c6ea32f7ad27f6f
Chloe Corrigan <ccorrigan@marginfactor.com>
2018-07-25 14:58:12 -0400

    token communication w/firebase for user auth and actions

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,6 +5,11 @@ import { Subject } from 'rxjs';
 
 export class AuthService {
 
+	//this token is needed to confirm user is logged in and able to save and fetch recipes.
+	//this will be set on signin in the response and then used in getToken()..
+	//NOTE: research better ways to handle this in the event of expired tokens.
+	token: string;
+
 	message = new Subject<string>();
 	error = new Subject<boolean>();
 
@@ -32,10 +37,34 @@ export class AuthService {
 	signinUser(email: string, password: string) {
 		firebase.auth().signInWithEmailAndPassword(email, password)
 		.then(
-			response => console.log(response)
+			// response => console.log(response)
+			//grab the token and save it
+			response => {
+				firebase.auth().currentUser.getIdToken()
+				.then(
+					(token: string)=> this.token = token
+				);
+			}
 		)
 		.catch(
 			error => console.log(error)
 		);
+	}
+
+	//get the token to be able to fetch and save recipes for logged in users!
+	getToken() {
+		//return what firebase gives us (does not give back token synchronously, this is done asynchronously b/c firebase does not only retrieve token from lcal storage, but will check if token is VALID -- if invalid due to expiration, it will reach out to backend automatically to get a new one)
+		//firebase.auth..getIdToken is a promise, so instead of returning it, will repeat the process and instead of waiting for it to finish, use the token set @ signin.  
+		//DANGER of token expiration -- error handling and/or alt method must be used to avoid this.
+		//this will be used on data-storage service.
+		firebase.auth().currentUser.getIdToken()
+		.then(
+			(token: string)=> this.token = token
+		);
+        
+        //return the token - may or may not be done before 'getToken' is finished, but it is already set on sign in.  Again, need to make sure token expiration is not a threat.
+        return this.token;
+
+        //now in data storage service, can call this method to access the token.
 	}
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -4,6 +4,7 @@ import { map, catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
 import { RecipeService } from '../recipe/recipe.service';
+import { AuthService } from '../auth/auth.service';
 import { RecipeModel } from '../recipe/recipe.model';
 
 
@@ -11,19 +12,29 @@ import { RecipeModel } from '../recipe/recipe.model';
 @Injectable()
 export class DataStorageService {
 
-	//inject http service & recipe service
-	constructor(private http: Http, private recipeService: RecipeService) {}
+	//inject http service & recipe service, & auth service for token check
+	constructor(private http: Http, private recipeService: RecipeService, private authService: AuthService) {}
 
 	//used to save recipe data
 	saveRecipes() {
+    //the token used to allow access for signed in users
+     const token = this.authService.getToken();
      //create or overwrite data with put (using firebase here to store the data) --> /recipes.json 
      //saving or storing a copy of the recipes in the database
-     return this.http.put('https://ng-recipe-master-book.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+     //add authentication by ?auth= plus token, as explained in fetchRecipes method below.
+     return this.http.put('https://ng-recipe-master-book.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
   }
 
   //with subscribing auto
   fetchRecipes() {
-    this.http.get('https://ng-recipe-master-book.firebaseio.com/recipes.json')
+
+    //first confirm the token for logged in users
+    const token = this.authService.getToken();
+    //(above) firebase's getIdToken is a promise, so could theoretically use a then block after getToken if only the getIdToken method was being returned.  However, the issue with that is that the token which will be available in that response method will not be accessible outside of it, so it can be used in the get function.  And the get function cannot be used inside of it, because if the observable wanted to be returned, it wouldn't be available b/c would be in a callback.  So, SEE AUTH service 'token' resolution.
+
+    //in order to authenticate to backend, need to add a query parameter, auth (recognized by firebase), to the route and set it equal to the token retrieved  
+    //this sends the token as apart of the url so that firebase can identify the user
+    this.http.get('https://ng-recipe-master-book.firebaseio.com/recipes.json?auth=' + token)
     //before subscribing, extract data and make sure there is an ingredients property 
     .pipe(map(
     	(response: Response)=> {
